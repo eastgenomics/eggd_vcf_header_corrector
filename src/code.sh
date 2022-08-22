0f27e3ce5f7b63fdfdcf76037b412bbcb093b390
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-08-22 15:54:40 +0100

    Get bcftools from existing asset instead of building from executable. Add output paths and upload commands.

--- src/code.sh
+++ src/code.sh
@@ -20,34 +20,12 @@ main() {
     echo "Value of mutect2_input: '$mutect2_input'"
     echo "Value of cgppindel_input: '$cgppindel_input'"
 
-    # The following line(s) use the dx command-line tool to download your file
-    # inputs to the local file system using variable names for the filenames. To
-    # recover the original filenames, you can use the output of "dx describe
-    # "$variable" --name".
+    set -e -x -v -o pipefail
+
+	export BCFTOOLS_PLUGINS=/usr/local/libexec/bcftools/
 
     dx download "$mutect2_input" -o mutect2_input
-
     dx download "$cgppindel_input" -o cgppindel_input
-
-    # Fill in your application code here.
-    #
-    # To report any recognized errors in the correct format in
-    # $HOME/job_error.json and exit this script, you can use the
-    # dx-jobutil-report-error utility as follows:
-    #
-    #   dx-jobutil-report-error "My error message"
-    #
-    # Note however that this entire bash script is executed with -e
-    # when running in the cloud, so any line which returns a nonzero
-    # exit code will prematurely exit the script; if no error was
-    # reported in the job_error.json file, then the failure reason
-    # will be AppInternalError with a generic error message.
-
-    # The following line(s) use the dx command-line tool to upload your file
-    # outputs after you have created them on the local file system.  It assumes
-    # that you have used the output field name for the filename for each output,
-    # but you can change that behavior to suit your needs.  Run "dx upload -h"
-    # to see more options to set metadata.
 
     # add tags required for openCGA
 	# write these to separate filtered but unannotated VCFs for uploading
@@ -87,4 +65,11 @@ main() {
     # does.
     dx-jobutil-add-output mutect2_output "$mutect2_output" --class=file
     dx-jobutil-add-output cgppindel_output "$cgppindel_output" --class=file
+
+    # Upload the outputs
+    mkdir -p ~/out
+    mv ~/"${mutect2_output}" ~/out/"${mutect2_output}"
+    mv ~/"${cgppindel_output}" ~/out/"${cgppindel_output}"
+    dx-upload-all-outputs --parallel
+    mark-success
 }
